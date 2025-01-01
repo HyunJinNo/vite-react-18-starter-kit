f2dfeaa10cafd499e0aee1b46ea17935ec6a11e0
HyunJinNo <nhj9831@naver.com>
2025-01-01 11:28:16 +0900

    feat: 환경변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import "./styles/App.css";
 const App = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-4">
-      <h1 className="text-5xl">Vite React Starter Kit</h1>
-      <p className="text-xl text-gray-500">{version}</p>
+      <h1 className="text-5xl">Vite React 18 Starter Kit</h1>
+      <p className="text-3xl">{version}</p>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
       </p>
+      <p>{import.meta.env.VITE_NUM ?? 1}</p>
     </div>
   );
 };
